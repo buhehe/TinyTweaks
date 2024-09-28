f13fde1c52024378add65f75649adc634e5eec0d
buhehe <angilscarl@gmail.com>
2024-09-28 19:43:07 +0800

    7.0 update

--- TinyTweaks/Tweaks/BetterTarget.cs
+++ TinyTweaks/Tweaks/BetterTarget.cs
@@ -5,9 +5,18 @@ using TinyTweaks.Utils;
 using Dalamud.Game.Command;
 using Dalamud.Game.ClientState.Objects.Types;
 using GameObjectStruct = FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject;
+using System.Collections.Generic;
 
 namespace TinyTweaks.Tweaks
 {
+    internal class GameObjectList
+    {
+        public uint objectId;
+        public IGameObject? gameObject;
+        public float distance;
+
+    }
+
     internal class BetterTarget : ITweak
     {
         private const string CommandName = "/btarget";
@@ -27,26 +36,53 @@ namespace TinyTweaks.Tweaks
 
         private unsafe void OnCommand(string command, string args)
         {
-            var searchTerms = args.Split(" ");
+            //create gameObjectList and items
+            List<GameObjectList> gameObjectList = new List<GameObjectList>();
+            GameObjectList gameObjectListItem;
 
+            //Original code
             IGameObject? closestMatch = null;
             var closestDistance = float.MaxValue;
             var player = Svc.ClientState.LocalPlayer;
-            if (player == null) return;
+            var searchTerms = args.Split(" ");
+
+
+            //parse String args
+            int numericValue;
+            var tryNumber = searchTerms.Last();
+            bool isNumber = int.TryParse(tryNumber, out numericValue);
+            if (isNumber)
+            {
+                searchTerms = searchTerms.SkipLast(1).ToArray();
+            }
+            else
+            {
+                numericValue = 1;
+            }
+
+            //Original code
             foreach (var actor in Svc.Objects)
             {
                 if (actor == null) continue;
-                var valueFound = searchTerms.Any(searchTerm => actor.Name.TextValue.Contains(searchTerm, StringComparison.InvariantCultureIgnoreCase));
+                var valueFound = searchTerms.Any(searchName => actor.Name.TextValue.ToLowerInvariant().Contains(searchName.ToLowerInvariant()));
                 if (valueFound && ((GameObjectStruct*)actor.Address)->GetIsTargetable())
                 {
                     var distance = Vector3.Distance(player.Position, actor.Position);
+
+                    //gameObjectListItem add matched item
+                    gameObjectListItem = new GameObjectList();
+                    gameObjectListItem.gameObject = actor;
+                    gameObjectListItem.distance = distance;
+                    gameObjectListItem.objectId = actor.EntityId;
+                    gameObjectList = gameObjectList.Append(gameObjectListItem).ToList();
+
+                    //Original code
                     if (closestMatch == null)
                     {
                         closestMatch = actor;
                         closestDistance = distance;
                         continue;
                     }
-
                     if (closestDistance > distance)
                     {
                         closestMatch = actor;
@@ -54,9 +90,19 @@ namespace TinyTweaks.Tweaks
                     }
                 }
             }
+
             if (closestMatch != null)
             {
-                Svc.Targets.Target = closestMatch;
+                if (numericValue == 1)
+                {
+                    //Original code
+                    Svc.Targets.Target = closestMatch;
+                }
+                else
+                {
+                    var sorted = gameObjectList.OrderBy(p => p.distance).ThenBy(p => p.objectId).ToList();
+                    Svc.Targets.Target = sorted[numericValue - 1].gameObject;
+                }
             }
         }
     }
